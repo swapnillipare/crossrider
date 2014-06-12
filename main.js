cafbdc94d027d01ffec0e064e355db159cccfea8
Swapnil Lipare <swapnil.lipare88@gmail.com>
2014-06-12 17:03:51 +0530

    Update main.js
    loggedInUser global variable added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+var loggedInUser = '';
 var b = function(){
 	alert('main js loaded now...');
 	
 	var UIObj = UIHandlerObj();
-	UIObj.get.user_email();
+	loggedInUser = UIObj.get.user_email();
 	console.log("In main Function :: "+UIHandlerObj().get.user_email());
 	alert(UIObj.get.user_email()+"  Main Js");
 	
