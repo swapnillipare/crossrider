cce9659918dbccf69c41f0fcfd743c3a98c17b9d
Swapnil Lipare <swapnil.lipare88@gmail.com>
2014-06-13 13:28:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ var b = function(){
 	loggedInUser = UIObj.get.user_email();
 	console.log("In main Function :: "+UIHandlerObj().get.user_email());
 	alert(UIObj.get.user_email()+"  Main Js");
-	
+	loggedInUserG = loggedInUser;
 };
